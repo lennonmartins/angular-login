f1d33a333baa1e1af15b1d3d4b320f1a84ec5f7d
martinsLennon <lennonmcarlos@gmail.com>
2024-08-06 16:30:04 -0400

    feat: Adição do estilo do layout

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-default-login-layout',
   templateUrl: './default-login-layout.component.html',
+  imports: [],
   styleUrls: ['./default-login-layout.component.scss'],
   standalone: true
 })
 export class DefaultLoginLayoutComponent {
-
+    @Input()  title: string = "";
+    @Input()  primaryBtnText: string = "";
+    @Input()  secondaryBtnText: string = "";
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { DefaultLoginLayoutComponent } from 'src/app/components/default-login-layout/default-login-layout.component';
 
 @Component({
@@ -6,8 +7,15 @@ import { DefaultLoginLayoutComponent } from 'src/app/components/default-login-la
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
   standalone: true,
-  imports: [DefaultLoginLayoutComponent]
+  imports: [DefaultLoginLayoutComponent, ReactiveFormsModule]
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
 
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
+  }
 }
